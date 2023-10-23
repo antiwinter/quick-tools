330bdc289f6d83f0867f6eb6067fdd7d3ebe7219
antiwinter <antiwinter@126.com>
2023-10-23 11:50:24 +0800

    add relative path

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,11 @@ import { Router } from 'preact-router'
 import { routes, Home } from './routes'
 import './app.css'
 
+//get base from ../vite.config.js
 export function App() {
   return (
     <div>
-      <Router>
+      <Router base={__BASE_URL__}>
         <Home path="/" />
         {routes.map(({ path, component: RouteComponent }) => (
           <RouteComponent path={path} />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
+let base = process.env.NODE_ENV === 'production' ? '/quick-tools/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  loader: { '.js': 'jsx' }
+  loader: { '.js': 'jsx' },
+  base,
+  define: {
+    __BASE_URL__: JSON.stringify(base)
+  }
 })
